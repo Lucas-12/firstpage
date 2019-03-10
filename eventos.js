2a55318716fe02fa7ab0169ab5d9458bb3cbc209
Lucas Diaz <lucascicba@gmail.com>
2019-03-09 21:41:08 -0300

    agregando enventos

--- eventos.js
+++ eventos.js
@@ -1,5 +1,12 @@
-const girarIcono =  document.getElementById('girar');
-girarIcono.classList.add('animated', 'flip');
+function anima(){
+    document.getElementById('girar').classList.add('animated', 'flip');
+}
+
+function animac(){
+    document.getElementById('giro').classList.add('animated', 'flip');
+}
+
 
 const girarLogo =  document.querySelector('.logo');
 girarLogo.classList.add('animated', 'flipInY');
+
